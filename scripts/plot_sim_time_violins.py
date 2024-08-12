3175aaae87802d6184de30637e1996c021798b3c
ljn94408 <jliu812@aucklanduni.ac.nz>
2024-08-12 16:10:45 +1200

    Create plot_sim_time_violins.py
    the violin plot for realization time both models by both SGVB and VNPC methods

---
+++ scripts/plot_sim_time_violins.py
@@ -0,0 +1,76 @@
+import pandas as pd
+import paths
+import matplotlib.pyplot as plt
+from matplotlib.patches import Patch
+from matplotlib import ticker
+
+
+
+
+def add_violin_plot(ax, data, positions, colors, legend_labels=None, legend_loc='upper right'):
+    """
+    Adds a violin plot to the given axis.
+
+    Parameters:
+    - ax: The Matplotlib axis to which the violin plot will be added.
+    - data: List of datasets for the violin plots.
+    - positions: List of positions for the violins.
+    - colors: List of colors for the violins.
+    - legend_labels: List of labels for the legend.
+    - legend_loc: Location of the legend on the plot.
+    """
+    parts = ax.violinplot(data, positions=positions, showmeans=False, showmedians=True)
+
+    # Customize violins
+    for i, pc in enumerate(parts['bodies']):
+        pc.set_facecolor(colors[i])
+        pc.set_linewidth(0)
+        pc.set_alpha(0.5)
+
+    for partname in ('cbars', 'cmins', 'cmaxes', 'cmedians'):
+        vp = parts[partname]
+        vp.set_edgecolors(colors)
+        vp.set_linewidth(1.5)
+
+    # Add legend
+    if legend_labels:
+        legend_patches = [Patch(facecolor=f'C{i}', label=label) for i, label in enumerate(legend_labels)]
+        ax.legend(handles=legend_patches, loc=legend_loc)
+
+    # Customize axes
+    ax.set_xticks([1.25, 3.25, 5.25])
+    ax.set_xticklabels(['256', '512', '1024'])
+    ax.set_ylabel('Time')
+    ax.xaxis.set_minor_locator(ticker.NullLocator())
+    ax.yaxis.set_minor_locator(ticker.NullLocator())
+
+
+
+def main():
+    # First panel: Original violin plot
+    colors = ['C1', 'C0'] * 3
+    legend_labels = ['VNPC', 'VB']
+
+    positions = [1.5, 1, 3.5, 3, 5.5, 5]
+
+    # Load data
+    violin_data1 = pd.read_csv(f"{paths.data}/time_var2_SGVB_VNPC.csv").values
+    violin_data2 = pd.read_csv(f"{paths.data}/time_vma1_SGVB_VNPC.csv").values
+
+
+    fig, axs = plt.subplots(nrows=2, ncols=1, sharex=True)
+    add_violin_plot(axs[0], violin_data1, positions, colors, legend_labels)
+    add_violin_plot(axs[1], violin_data2, positions, colors)
+
+
+    # Annotate subplots
+    axs[0].text(0.02, 0.98, 'a)', transform=axs[0].transAxes, fontsize=10, verticalalignment='top', horizontalalignment='left')
+    axs[1].text(0.02, 0.98, 'b)', transform=axs[1].transAxes, fontsize=10, verticalalignment='top', horizontalalignment='left')
+    axs[1].set_xlabel("Data Length")
+    plt.subplots_adjust(hspace=0.)  # Adjust as needed
+    plt.savefig(f"{paths.figures}/sim_time_violins.pdf", dpi=300)
+    plt.close()
+
+
+if __name__ == "__main__":
+    main()
