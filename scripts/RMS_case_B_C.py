25e691e77f5f0b99c71331cf3a5d169899f7596f
ljn94408 <jliu812@aucklanduni.ac.nz>
2024-09-11 05:51:57 +1200

    Update RMS_case_B_C.py

--- scripts/RMS_case_B_C.py
+++ scripts/RMS_case_B_C.py
@@ -1,47 +1,88 @@
 import h5py
 import numpy as np
 import paths
+import pandas as pd
+from scipy import interpolate as interp
+
+# upload the true psd data
+file_path = f'{paths.data}/ET(1).txt'
+ET_1 = pd.read_csv(file_path, delim_whitespace=True, header=None).values
+
+file_path = f'{paths.data}/Peak10Hz_new.txt'
+Peak10Hz = pd.read_csv(file_path, delim_whitespace=True, header=None).values
+
+file_path = f'{paths.data}/Peak50Hz_new.txt'
+Peak50Hz = pd.read_csv(file_path, delim_whitespace=True, header=None).values
+
+file_path = f'{paths.data}/Peak90Hz_new.txt'
+Peak90Hz = pd.read_csv(file_path, delim_whitespace=True, header=None).values
+
+x_channel_real = ((ET_1[:, 1] ** 2 + Peak10Hz[:, 1] ** 2 + Peak50Hz[:, 1] ** 2)) / 2
+y_channel_real = ((ET_1[:, 1] ** 2 + Peak10Hz[:, 1] ** 2 + Peak90Hz[:, 1] ** 2)) / 2
+z_channel_real = ((ET_1[:, 1] ** 2 + Peak50Hz[:, 1] ** 2 + Peak90Hz[:, 1] ** 2)) / 2
 
 q = 10 ** 22 / 1.0
-freq_original = 1024
-def compute_average_RMS(file_path, dataset_prefix):
-    
-    with h5py.File(file_path, 'r') as f:
-        spec_mat_median = f[f'{dataset_prefix}_spec_mat_median_XYZ'][:]
-        spec_mat_lower = f[f'{dataset_prefix}_spec_mat_lower_XYZ'][:]
-        spec_mat_upper = f[f'{dataset_prefix}_spec_mat_upper_XYZ'][:]
+time_interval = 2000
+nchunks = 125
+required_part = 128
+total_len = 4096000
 
-    spec_mat_median = np.real(spec_mat_median)
-    spec_mat_median = spec_mat_median/q**2/(freq_original/0.5)
+Ts = 1 / (total_len / time_interval)
+freq_original = np.fft.fftfreq(int(total_len / nchunks), Ts)
+
+n = int(total_len / nchunks)
+if np.mod(n, 2) == 0:
+    # n is even
+    freq_original = freq_original[0:int((n / 2))]
+else:
+    # n is odd
+    freq_original = freq_original[0:int((n - 1) / 2)]
+
+freq_range = total_len / time_interval / 2
+freq = freq_original[0:int(required_part / freq_range * freq_original.shape[0])]
+freq_true = ET_1[:, 0]
+
+true_s = [
+        interp.interp1d(freq_true, x_channel_real, kind='cubic', bounds_error=False, fill_value='extrapolate')(freq),
+        interp.interp1d(freq_true, y_channel_real, kind='cubic', bounds_error=False, fill_value='extrapolate')(freq),
+        interp.interp1d(freq_true, z_channel_real, kind='cubic', bounds_error=False, fill_value='extrapolate')(freq)
+    ]
+
+#frequency band start at 5Hz
+freq_start = int(5/required_part * freq.shape[0])
+
+def calculate_rmsd(spec_file_path, q, freq_original, true_s):
+    
+    with h5py.File(f'{paths.data}/{spec_file_path}', 'r') as f:
+        spec_mat_median = f['ETnoise_correlated_GP_spec_mat_median_XYZ'][:]
+
+    spec_mat_median = spec_mat_median / q**2 / (freq_original[-1] / 0.5)
     p = spec_mat_median.shape[-1]
 
-    RMS = []
-    for i in range(p):
-        for j in range(p):
-            if i == j:
-                
-                RMS_ij = np.sqrt(np.mean(np.real(spec_mat_median[..., i, j])**2))
-            elif i < j:
-                
-                RMS_ij = np.sqrt(np.mean(np.real(spec_mat_median[..., i, j])**2))
-            else:
-                
-                RMS_ij = np.sqrt(np.mean(np.imag(spec_mat_median[..., i, j])**2))
+    rmsd_channels = []
+    for i in range(p):   
+        rmsd_channel = np.sqrt(np.mean((np.real(spec_mat_median[..., i, i][freq_start:]) - true_s[i][freq_start:]) ** 2))
+        rmsd_channels.append(rmsd_channel)
 
-            RMS.append(RMS_ij)
+    return np.mean(rmsd_channels)
 
-    average_RMS = np.mean(RMS)
-    return average_RMS
+RMSD_case_B = calculate_rmsd('ETnoise_uncorrelated_GP_uniform_spec_matrices_XYZ.hdf5', q, freq_original, true_s)
+RMSD_case_C = calculate_rmsd('ETnoise_no_cross_uncorrelated_GP_uniform_spec_matrices_XYZ.hdf5', q, freq_original, true_s)
 
-# Case B
-file_path_B = f'{paths.data}/ETnoise_uncorrelated_GP_uniform_spec_matrices_XYZ.hdf5'
-average_RMS_B = compute_average_RMS(file_path_B, 'ETnoise_correlated_GP')
-print(f'Average RMS for case B: {average_RMS_B}')
+print(f'RMSD Case B: {RMSD_case_B}')
+print(f'RMSD Case C: {RMSD_case_C}')
 
-# Case C
-file_path_C = f'{paths.data}/ETnoise_no_cross_uncorrelated_GP_uniform_spec_matrices_XYZ.hdf5'
-average_RMS_C = compute_average_RMS(file_path_C, 'ETnoise_correlated_GP')
-print(f'Average RMS for case C: {average_RMS_C}')
+
+
+
+
+
+
+
+
+
+
+
 
 
 
